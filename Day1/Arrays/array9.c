8e79cea51979c7952af3a118cda8940d8a4d4f5d
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-30 02:09:58 +0600

    Day1

--- Day1/Arrays/array9.c
+++ Day1/Arrays/array9.c
@@ -1,18 +1,46 @@
 // Find the leader Elements
 // leader element in an array is greater than all the elements to its right ,
 #include <stdio.h>
+#define size 6
+
 int main()
 {
-    int num[5];
-    for (int i = 0; i < 5; i++)
+    // 16 17 4 3 5 2
+    // const int size = 6; i have to write #define variable constant_value_that_i_want_to_assign_in_it (and no semicolone or ending symbole just the constant value i have to insert)
+    int arr[size];
+    for (int i = 0; i < size; i++)
     {
-        scanf("%d", &num[i]);
+        scanf("%d", &arr[i]);
     }
-    int target = num[0];
-    for (int i = 0; i < 5; i++){
-        for (int j = i; j < 5; j++){
-        
+
+    for (int j = 0; j < size; j++)
+    {
+        int count = 0, target = 0;
+        for (int k = (j + 1); k < size; k++)
+        {
+            target = j + 1;
+            if (arr[j] > arr[k])
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (count == (size - target))
+        {
+            printf("%d", arr[j]);
+            if (j != (size - 1))
+            {
+                printf(", ");
+            }
+        }
+        if (j == (size - 1))
+        {
+            printf("%d\n", arr[j]);
         }
     }
-return 0;
+
+    return 0;
 }
