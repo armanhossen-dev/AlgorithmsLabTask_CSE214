55f6568cecb04c01f591f0cadf34cf99d0510c2a
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-29 06:58:16 +0600

    Day2

--- Day2/BinarySearch/BST_code.cpp
+++ Day2/BinarySearch/BST_code.cpp
@@ -11,6 +11,7 @@ typedef struct Node{
     }
 }bst;
 
+
 //insert value in bst also create new node
 bst *insertInBst(bst *root, int val){
     if(root == NULL){
@@ -24,15 +25,17 @@ bst *insertInBst(bst *root, int val){
     return root;
 }
 
+
 //inorder prints in Ascending order: [1, 2, 3, 4, 5]
 void inOrder(bst *root){
     if(root == NULL){
         return;
     }
     inOrder(root->left);
-    cout<< root->data <<' ';
+    cout<< root->data <<" ";
     inOrder(root->right);
 }
+
 
 //search in bst
 bst *searchInBst(bst *root, int key){
@@ -50,6 +53,41 @@ bst *searchInBst(bst *root, int key){
 }
 
 
+//function for inOrder Successor finding 
+bst* inOrderSucc(bst* root){
+    bst* curr = root;
+    while(curr!=NULL  && curr->left != NULL){
+        curr = curr->left;
+    }
+    return curr;
+}
+
+
+//delete node also the bst's every node will follow the rule to be bst
+bst* deleteInBst(bst* root, int value){
+    if(value < root->data){
+        root->left = deleteInBst(root->left, value);
+    }else if(value > root->data){
+        root->right = deleteInBst(root->right, value);
+    }else{
+        if(root->left == NULL){
+            bst* temp = root->right;
+            free(root);
+            return temp;
+        }else if(root->right == NULL){
+            bst* temp = root->left;
+            free(root);
+            return temp;
+        }
+        //case 3
+        bst* temp = inOrderSucc(root->right);
+        /*root et 2 ta child node , right e jeta sob teke soto setai hobe in order successor*/
+        root->data = temp->data;
+        root->right= deleteInBst(root->right, temp->data);
+    }
+    return root;
+}
+
 
 int main(){
     bst *root = NULL;
@@ -63,6 +101,9 @@ int main(){
 
     cout << "Ascending order / Inorder : ";
     inOrder(root);
+    cout << endl;
+
+
     int numTofind;
     cout<< "\nEnter the number you want to search in the BST : ";
     cin>> numTofind;
@@ -72,6 +113,16 @@ int main(){
         cout << "The number exists\n";
     }
 
+
+    //cout << endl << endl;//yes it works, it gives two new blank lines
+    //to check delete function working or not
+    cout << endl;
+    int del;
+    inOrder(root); cout << endl;
+    cout << "Enter the value you want to delete : ";
+    cin >> del;
+    root = deleteInBst(root, del);
+    inOrder(root); cout << endl;
 
     return 0;
 }
